277db250457c3f211282d623514c5758a2de8bc9
Pfesi van Zyl <pfesi24@gmail.com>
2026-03-02 11:40:02 +0200

    added rtd theme and myst parser

--- src/docs/source/conf.py
+++ src/docs/source/conf.py
@@ -5,24 +5,64 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+from pathlib import Path
 
 project = 'dran'
 copyright = '2026, pfesesani van zyl'
 author = 'pfesesani van zyl'
-release = '0.1'
+release = '0.1.0'
+
+VERSION: str = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+_ROOT = Path(__file__).resolve().parent
+print('ROOT: ', _ROOT)
 
-extensions = []
+extensions = [
+    # 'recommonmark', # create .md files
+    # Auto-generate section labels.
+    'sphinx.ext.autosectionlabel',
+    
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+    
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autosummary',
+    
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    
+    'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon',
+    'myst_parser',
+    'sphinx.ext.duration',
+    # 'nbsphinx'
+]
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme' #'alabaster'
 html_static_path = ['_static']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+source_parsers = {'.md': 'recommonmark.parser.CommonMarkParser'}
+
+# EPUB options
+epub_show_urls = 'footnote'
+
+def setup(app):
+    app.add_css_file("css/custom.css")
